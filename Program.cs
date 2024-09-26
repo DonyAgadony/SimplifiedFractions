7f1e2733e786c80081d53cf197ef244cba81e571
Daniel <donybof1@gmail.com>
2024-09-26 16:21:04 +0300

    Finished

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Simplify("4/6"));
+    }
+    public static string Simplify(string OGFraction)
+    {
+        string[] fractions = OGFraction.Split('/');
+        double UP = double.Parse(fractions[0]);
+        double down = double.Parse(fractions[1]);
+        for (double i = down; i > 0; i--)
+        {
+            if (UP % i == 0 && down % i == 0)
+            {
+                UP /= i;
+                down /= i;
+            }
+        }
+        return $"{UP}/{down}";
+
+    }
+}
